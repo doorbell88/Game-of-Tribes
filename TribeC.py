5d75e1438df4923ca7e81b46dfca8fd2fe4bf120
Kevin Klinkel <kgklinkel@gmail.com>
2016-12-16 19:36:40 -0700

    Add files via upload
    Added gameoflife_tribes.py, and the three Tribes that work together in a balanced way

---
+++ TribeC.py
@@ -0,0 +1,174 @@
+# GOL (Game of Life) Ruleset
+
+from random import randint, choice
+
+# A spider that performs operations on the whole board
+class CellChecker(object):
+	def __init__(self):
+		pass
+
+	# Count how many neighboring cells (within margin) are a certain symbol
+	def CountNeighbors(self, board, position, margin):
+		self.position = position
+
+		neighbors = {}
+		# neighbors = 0
+		for j in range(-margin, margin+1):
+			for i in range(-margin, margin+1):
+
+				# Do not check the current (center) cell
+				if j==0 and i==0:
+					pass
+
+				else:
+					y = self.position[0] + j
+					x = self.position[1] + i
+
+					# if going off the edge of the map, pass
+					if 	y < 1 or 				\
+						x < 1 or 				\
+						y > board.height-1 or 	\
+						x > board.width-1:
+							pass
+					else:
+						if board.previous[y][x] not in neighbors:
+							neighbors[ board.previous[y][x] ] = 0
+						neighbors[ board.previous[y][x] ] += 1
+
+		return neighbors
+
+	# Create randomized initial conditions
+	def random(self, board, fraction, **kwargs):
+		# Make a few random cells "on"
+		y_bounds = kwargs.get('y_bounds', [1, board.height-2])
+		x_bounds = kwargs.get('x_bounds', [1, board.width-2])
+		n=0
+		while n < ((y_bounds[1]-y_bounds[0]) * (x_bounds[1]-x_bounds[0]))/fraction :
+			y = randint(y_bounds[0], y_bounds[1])
+			x = randint(x_bounds[0], x_bounds[1])
+			board.previous[y][x] = self.symbol
+			n += 1
+
+	# Randomly move some random alive cells
+	def RandomMigration(self, board):
+		for y in range(1, board.height-1):
+			for x in range(1, board.width-1):
+				if randint(1,50) == 1:
+					j = choice([-1, 1])
+					i = choice([-1, 1])
+
+					# if previous spot is occupied
+					if board.previous[y][x] == '#':
+						# if adjacent spot is unoccupied
+						if board.next[y+j][x+i] == ' ':
+
+							# move to unoccupied spot
+							if randint(1,2) == 1:
+								board.previous[y][x] = ' '
+							board.next[y+j][x+i] = '#'
+
+	# Enforce GOL rules
+	def RunRules(self, board, rules):
+		for y in range(1, board.height-1):
+			for x in range(1, board.width-1):
+				rules(board, [y,x])
+
+
+
+###############################################################################################
+# TribeC Rules
+class TribeC(CellChecker):
+	def __init__(self, symbol, color):
+		CellChecker.__init__(self)
+		self.symbol = symbol
+		self.color = color
+
+
+	def Rules(self, board, position):
+		self.position = position	#[y,x]
+		self.board = board
+		y = self.position[0]
+		x = self.position[1]
+		margin = 3
+		friend = self.symbol
+		enemies = self.enemies
+		enemy_symbols = []
+
+
+		neighbors = self.CountNeighbors(board, position, margin)
+		alive_neighbors = neighbors.get(friend, 0)
+
+		alive_enemies = 0
+		for enemy in self.enemies:
+			alive_enemies += neighbors.get(enemy.symbol, 0)
+			enemy_symbols.append(enemy.symbol)
+
+
+
+		###############################################################################################
+		# TribeB -- Rules
+		"""	###########################################################################################
+		1. Any live cell with fewer than 2 live neighbors dies, as if caused by underpopulation.
+		2. Any live cell with 2 or 3 live neighbors lives on to the next generation.
+		3. Any live cell with more than 3 live neighbors dies, as if by overpopulation.
+		4. Any dead cell with exactly 3 live neighbors becomes a live cell, as if by reproduction.
+		""" ###########################################################################################
+		
+		# Rule 2, 3
+		if board.previous[y][x] == self.symbol:					# Previously alive
+			# if 10 <= alive_neighbors <= 31:
+			if 10 <= alive_neighbors <= 31:
+				board.next[y][x] = self.symbol
+				return True
+
+		# Rule 4
+		elif board.previous[y][x] != self.symbol:				# Previously dead or enemy
+			# if 14 <= alive_neighbors <= 22 and board.previous[y][x] == ' ':
+			if 14 <= alive_neighbors <= 22 and board.previous[y][x] == ' ':
+				board.next[y][x] = self.symbol
+				return True
+			else:
+				board.next[y][x] = board.previous[y][x]
+				return False
+
+
+		# # Rule 1, 3
+		# else:
+		# 	board.next[y][x] = board.previous[y][x]				# Under/Overpopulation
+		# 	return False
+
+
+		if alive_enemies >= 15:
+			for j in range(-margin, margin+1):
+				for i in range(-margin, margin+1):
+					if 	(y+j) > 2 and (x+i) > 2 and \
+						(y+j) < board.height-2 and (x+i) < board.width-2:
+							board.next[y+j][x+i] = self.symbol
+
+		#Enemy eating
+		elif board.previous[y][x] == self.symbol:
+			# if 12 <= alive_enemies <= 36:
+
+			number_to_eat = 0
+			if alive_enemies >= 5:
+				number_to_eat = alive_enemies
+
+				eat=0
+				while eat < number_to_eat:
+					n=0
+					j=0
+					i=0
+					# while board.next[y+j][x+i] != self.symbol and n<((margin*2)**2):
+					while board.next[y+j][x+i] not in self.enemies and n<(margin*3):
+						j = randint(-1,1)
+						i = randint(-1,1)
+						n+=1
+						if 	(y+j) > 1 and (x+i) > 1 and \
+							(y+j) < board.height-1 and (x+i) < board.width-1:
+								board.next[y+j][x+i] = self.symbol
+					eat += 1
+
+
+		###############################################################################################
+
+
---
+++ gameoflife_tribes.py
@@ -0,0 +1,171 @@
+# Game of Life
+
+from copy import deepcopy
+from time import sleep, time
+from subprocess import Popen, PIPE
+from termcolor import colored, cprint
+from random import randint, choice
+import os
+
+
+
+# get size of terminal
+#width
+stdout = Popen('tput cols', shell=True, stdout=PIPE).stdout
+WIDTH = int( stdout.read() )
+#height
+stdout = Popen('tput lines', shell=True, stdout=PIPE).stdout
+HEIGHT = int( stdout.read() ) - 1
+
+
+
+
+##### CLASSES #####
+
+# Display of the window
+class Window(object):
+	def __init__(self, border_color):
+		self.border_color = border_color
+		self.width = WIDTH
+		self.height = HEIGHT
+		self.size = (self.height * self.width)
+
+		self.box = []
+		self.previous = []
+		self.next = []
+
+		#create blank window box
+		for y in range(self.height):
+			self.box.append([" "] * self.width)
+
+
+		#draw window border
+		for y in range(0, self.height):
+			for x in range(0, self.width):
+				#top 
+				self.box[0][x] = colored( "=", self.border_color )
+				#bottom
+				self.box[self.height - 1][x] = colored( "=", self.border_color )
+				#left
+				self.box[y][0] = colored( "|", self.border_color )
+				#right
+				self.box[y][self.width - 1] = colored( "|", self.border_color )
+
+		# Make copies for iteration
+		self.previous = deepcopy(self.box)
+		self.next = deepcopy(self.box)
+
+
+	# Print "previous" to the terminal
+	def display(self):
+		# Clear screen and scrollback buffer
+		os.system("clear && printf '\e[3J' ")
+		# os.system("clear")
+
+		# print
+		for row in range( len(self.previous) ):
+			current_row = []
+			for col in range( len(self.previous[row]) ):
+				cell = self.previous[row][col]
+				current_row.append( colored(cell, colors.get(cell, 'green')) )
+			print "".join(current_row)
+			
+
+
+	# Refresh the previous and next boards
+	def refresh(self):
+		# Create template for next iteration
+		self.previous = deepcopy(self.next)
+		self.next = deepcopy(self.box)
+
+
+	# Create randomized initial conditions
+	def random(self, fraction):
+		# Make a few random cells "on"
+		n=0
+		while n < (self.height * self.width)/fraction :
+			y = randint(1, self.height-2)
+			x = randint(1, self.width-2)
+			self.previous[y][x] = '#'
+			n += 1
+
+	# Return the population of the map
+	def Census(self, tribe):
+		census = 0
+		for y in range(1, self.height-1):
+			for x in range(1, self.width-1):
+				if self.previous[y][x] == tribe.symbol:
+					census += 1
+		return census
+
+
+
+
+##################################################################
+# Import Tribes
+from TribeA import CellChecker, TribeA
+from TribeB import CellChecker, TribeB
+from TribeC import CellChecker, TribeC
+# from TribeD import CellChecker, TribeD
+##################################################################
+
+
+Grid = Window('red')
+
+T_A = TribeA('*', 'cyan')
+T_B = TribeB('#', 'magenta')
+T_C = TribeC('@', 'yellow')
+# T_D = TribeC('*', 'white')
+
+colors = 	{	T_A.symbol : T_A.color ,
+				T_B.symbol : T_B.color ,
+				T_C.symbol : T_C.color ,
+				# T_D.symbol : T_C.color
+			}
+
+
+# Grid.random(2)
+
+T_A.random(Grid, 4, y_bounds=[1, Grid.height/2])
+T_B.random(Grid, 4, x_bounds=[1, Grid.width/2])
+T_C.random(Grid, 4, y_bounds=[Grid.height*1/3, Grid.height-2], x_bounds=[Grid.width*1/3, Grid.width-2])
+
+T_A.random(Grid,8)
+T_B.random(Grid,6)
+T_C.random(Grid,4)
+
+T_A.enemies = [T_B, T_C]
+T_B.enemies = [T_C]
+T_C.enemies = [T_A]
+
+# Give preview of grid before starting
+Grid.display()
+sleep(1)
+
+
+# Infinite Loop
+turn = 1
+while True:
+
+	if turn%3 == 0:
+		T_A.RunRules(Grid, T_A.Rules)					# Game of Life rules
+		Grid.display()
+		Grid.refresh()
+		
+	elif turn%3 == 1:
+		T_B.RunRules(Grid, T_B.Rules)					# Game of Life rules
+		Grid.display()
+		Grid.refresh()
+
+	elif turn%3 == 2:
+		T_C.RunRules(Grid, T_C.Rules)					# Game of Life rules
+		Grid.display()
+		Grid.refresh()
+
+
+	# sleep(0.01)
+	turn += 1
+
+
+
+
